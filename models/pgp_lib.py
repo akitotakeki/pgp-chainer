dbf6193b767500db80e2b711b9a8c2b4ed8476e5
akitotakeki <takeki@hal.t.u-tokyo.ac.jp>
2018-04-15 15:04:02 +0900

    use function_node for PGP

--- models/pgp_lib.py
+++ models/pgp_lib.py
@@ -1,31 +1,102 @@
-import numpy as np
 import chainer
+from chainer.backends import cuda
+from chainer import function_node
+from chainer.utils import type_check
 
 
-def pgp(x, r=2):
-    in_batch, in_channel, in_height, in_width = x.data.shape
-    if not in_height % r == 0:
-        x = zero_pads(x, in_height % r, 2)
-    if not in_width % r == 0:
-        x = zero_pads(x, in_width % r, 3)
-    in_batch, in_channel, in_height, in_width = x.data.shape
-    out_batch, out_channel, out_height, out_width = int(in_batch * (r ** 2)), in_channel, int(in_height / r),  int(in_width / r)
-    h = x.reshape(in_batch, in_channel, out_height, r, out_width, r)
-    h = h.transpose(3, 5, 0, 1, 2, 4)
-    return h.reshape(out_batch, out_channel, out_height, out_width)
+class PGP(function_node.FunctionNode):
+
+    """Parallel Grid Pooling."""
+
+    def __init__(self, r=2):
+        self.r = r
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        type_check.expect(
+            in_types[0].dtype.kind == 'f',
+            in_types[0].ndim == 4
+        )
+
+    def forward(self, inputs):
+        X, = inputs
+        xp = cuda.get_array_module(X)
+        bsize, c, a, b = X.shape
+        if a % self.r:
+            X = zero_pads(X, a % self.r, 2)
+        if b % self.r:
+            X = zero_pads(X, b % self.r, 3)
+
+        bsize, c, a, b = X.shape
+        X = xp.reshape(
+            X, (bsize, c, a // self.r, self.r, b // self.r, self.r))
+        X = xp.transpose(X, (3, 5, 0, 1, 2, 4))
+        X = xp.reshape(
+            X, (self.r ** 2 * bsize, c, a // self.r, b // self.r))
+        return X,
+
+    def backward(self, indexes, grad_outputs):
+        gy, = grad_outputs
+        bsize, c, a, b = gy.shape
+        bsize //= self.r ** 2
+        gy = chainer.functions.reshape(gy, (self.r, self.r, bsize, c, a, b))
+        gy = chainer.functions.transpose(gy, (2, 3, 4, 0, 5, 1))
+        gy = chainer.functions.reshape(gy, (bsize, c, a * self.r, b * self.r))
+        return gy,
 
 
-def pgp_inv(x, r=2):
-    in_batch, in_channel, in_height, in_width = x.data.shape
-    out_batch, out_channel, out_height, out_width = int(in_batch / (r ** 2)), in_channel, r * in_height, r * in_width
-    h = x.reshape(r, r, out_batch, in_channel, in_height, in_width)
-    h = h.transpose(2, 3, 4, 0, 5, 1)
-    return h.reshape(out_batch, out_channel, out_height, out_width)
+def pgp(X, r):
+    return PGP(r).apply((X,))[0]
+
+
+class PGPInverse(function_node.FunctionNode):
+
+    """The inverse of Parallel Grid Pooling."""
+
+    def __init__(self, r=2):
+        self.r = r
+
+    def check_type_forward(self, in_types):
+        type_check.expect(in_types.size() == 1)
+        type_check.expect(
+            in_types[0].dtype.kind == 'f',
+            in_types[0].ndim == 4
+        )
+
+    def forward(self, inputs):
+        X, = inputs
+        xp = cuda.get_array_module(X)
+        bsize, c, a, b = X.shape
+        bsize //= self.r ** 2
+        X = xp.reshape(X, (self.r, self.r, bsize, c, a, b))
+        X = xp.transpose(X, (2, 3, 4, 0, 5, 1))
+        X = xp.reshape(X, (bsize, c, a * self.r, b * self.r))
+        return X,
+
+    def backward(self, indexes, grad_outputs):
+        gy, = grad_outputs
+        bsize, c, a, b = gy.shape
+        if a % self.r:
+            gy = zero_pads(gy, a % self.r, 2)
+        if b % self.r:
+            gy = zero_pads(gy, b % self.r, 3)
+
+        bsize, c, a, b = gy.shape
+        gy = chainer.functions.reshape(
+            gy, (bsize, c, a // self.r, self.r, b // self.r, self.r))
+        gy = chainer.functions.transpose(gy, (3, 5, 0, 1, 2, 4))
+        gy = chainer.functions.reshape(
+            gy, (self.r ** 2 * bsize, c, a // self.r, b // self.r))
+        return gy,
+
+
+def pgp_inv(X, r):
+    return PGPInverse(r).apply((X,))[0]
 
 
 def zero_pads(x, pad, where):
-    sizes = list(x.data.shape)
+    sizes = list(x.shape)
     sizes[where] = pad
-    xp = chainer.cuda.get_array_module(x)
-    pad_mat = xp.zeros(sizes, dtype=np.float32)
+    xp = cuda.get_array_module(x)
+    pad_mat = xp.zeros(sizes, dtype=x.float32)
     return xp.concatenate((pad_mat, x), axis=where)
